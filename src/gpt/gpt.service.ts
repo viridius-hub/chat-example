dbee219ed7d4d01726fdd8e5eba0d56d1d74d856
viridius-hub <smiledie-hub@yandex.ru>
2023-11-29 09:23:44 +0500

    fix

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,14 +1,12 @@
 import {Injectable} from '@nestjs/common';
 import {HttpService} from "@nestjs/axios";
-import {Stream} from "typeorm";
 
 @Injectable()
 export class GptService {
     constructor(private readonly httpService: HttpService) {
     }
 
-    private org = "org-fYC1uVemh7NrTH0ZVfn6N1EV"
-    private apiKey = "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"
+    private apiKey = process.env.OPENAI_API_KEY
     private baseURL = "https://api.openai.com/v1/chat/completions"
 
     async translate(dto: { text: string, lang: string }): Promise<string> {
